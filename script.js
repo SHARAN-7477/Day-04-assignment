303662acf650845de1901712c1833c819f978ec6
SHARAN M <121308881+SHARAN-7477@users.noreply.github.com>
2023-01-03 16:44:56 +0530

    Add files via upload
    sorry for late submission because of health  issue

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
-// 1. How to compare 2 JSON have the same properties without order ?
+// 1.Question :
+// use the rest of countries in API URL
 
-let object1 = {
-    name: 'Sharan',
-    age: '22'
-  };
-      
-  let object2 = {
-    age: '22',
-    name: 'Sharan'
+let xhr = new XMLHttpRequest();
+console.log(xhr);
+xhr.open("GET","https://restcountries.com/v2/all");
 
-  };
-      
-  JSON.stringify(object1) === JSON.stringify(object2)
-  // false
-      
-  _.isEqual(object1, object2)
-  // true
-  console.log(object1 === object2)
+// setup our listner to process the completet
+xhr.onload=function(){
+    if(xhr.status==200 && xhr.status<300){
+        let jsonobj = JSON.parse(this.response);
 
-  // OUTPUT : false
- 
+        for (let i = 0; i < jsonobj.length; i++){
+            console.log(jsonobj[i].flags.png)
+        
+        }
 
-  
-  
+    }
+}
+xhr.send()
+
+// 2.Use the rest of same countries and print all countries name ,region,sub-region and population
+
+let xhr = new XMLHttpRequest();
+console.log(xhr);
+xhr.open("GET","https://restcountries.com/v2/all");
+
+// setup our listner to process the completet
+xhr.onload=function(){
+    if(xhr.status==200 && xhr.status<300){
+        let jsonobj = JSON.parse(this.response);
+
+        for (let i = 0; i < jsonobj.length; i++){
+            console.log(jsonobj[i].name)
+            console.log(jsonobj[i].region)
+            console.log(jsonobj[i].subregion)
+            console.log(jsonobj[i].population)
+
+        
+        }
+
+    }
+}
+xhr.send()
+
